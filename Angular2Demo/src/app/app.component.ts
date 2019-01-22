ee08ce379b71b4031e93222caf905f7cd23e4841
Swaraj <ghoshswaraj2k@gmail.com>
2019-01-22 12:07:06 +0530

    Added nested employee component and removed useless js file

--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -2,9 +2,14 @@ import { Component } from "@angular/core"
 
 @Component({
   selector: 'my-app',
-  templateUrl: 'app/app.component.html'
+    template: `
+                    <div>
+                    <h1>{{pageHeader}}</h1>
+                    <my-employee></my-employee>
+                    </div>
+                    `
 })
 
 export class AppComponent {
-  name: string = "Angular";
+  pageHeader: string = "Employee Details";
 }
---
+++ Angular2Demo/src/app/employee/employee.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: 'my-employee',
+    templateUrl: 'app/employee/employee.component.html'
+})
+
+export class EmployeeComponent {
+    firstName: string = 'Swaraj';
+    lastName: string = 'Ghosh';
+    gender: string = 'Male';
+    age: number = 33;
+}

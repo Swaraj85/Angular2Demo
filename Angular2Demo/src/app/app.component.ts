98370c2d49351d7831dcbc2b5e90705cd2993434
Swaraj <ghoshswaraj2k@gmail.com>
2019-01-22 14:22:49 +0530

    Example of property binding

--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -2,14 +2,29 @@ import { Component } from "@angular/core"
 
 @Component({
   selector: 'my-app',
-    template: `
+  template: `
                     <div>
-                    <h1>{{pageHeader}}</h1>
-                    <my-employee></my-employee>
+                      <h1>{{ pageHeader ? pageHeader: 'No Header' }}</h1>
+                      <h2>{{getFullName()}}</h2>
+                      <img [src]='imagePath'/>
+                      <my-employee></my-employee>
+                      <span bind-innerHtml='pageHeader'></span>
+                      <button [disabled]='isDisabled'>Click Me</button>
+                      <button [disabled]='isDisabled'>Click Me twice</button>
                     </div>
                     `
 })
 
 export class AppComponent {
-  pageHeader: string = "Employee Details";
+  pageHeader: string = 'This is page header';
+  imagePath: string = 'http://www.pragimtech.com/Images/Logo.jpg';
+
+  firstName: string = 'Tom';
+  lastName: string = 'Hopkins';
+
+  isDisabled: boolean= false;
+
+  getFullName(): string {
+    return this.firstName + ' ' + this.lastName;
+  }
 }

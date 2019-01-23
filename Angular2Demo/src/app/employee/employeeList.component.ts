822b0677d9f8a500b59e49dedb12d677f9b7b6da
Swaraj <ghoshswaraj2k@gmail.com>
2019-01-23 13:17:20 +0530

    Clear the clutter for new branch

--- Angular2Demo/src/app/employee/employeeList.component.ts
+++ Angular2Demo/src/app/employee/employeeList.component.ts
@@ -15,26 +15,9 @@ export class EmployeeListComponent {
             { code: 'emp01', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '01/25/1985' },
             { code: 'emp02', name: 'Tom2', gender: 'Male', annualSalary: 5500, dateOfBirth: '01/25/1985' },
             { code: 'emp03', name: 'Tom3', gender: 'Male', annualSalary: 5500, dateOfBirth: '01/25/1985' },
-            { code: 'emp04', name: 'Tom4', gender: 'Female', annualSalary: 5500, dateOfBirth: '01/25/1985' }
-        ];
-    }
-
-    getEmployees(): void {
-        this.employees = [
-            { code: 'emp01', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '01/25/1985' },
-            { code: 'emp02', name: 'Tom2', gender: 'Male', annualSalary: 5500, dateOfBirth: '01/25/1985' },
-            { code: 'emp03', name: 'Tom3', gender: 'Male', annualSalary: 5500, dateOfBirth: '01/25/1985' },
             { code: 'emp04', name: 'Tom4', gender: 'Female', annualSalary: 5500, dateOfBirth: '01/25/1985' },
             { code: 'emp05', name: 'Tom4', gender: 'Female', annualSalary: 5500, dateOfBirth: '01/25/1985' },
             { code: 'emp06', name: 'Tom4', gender: 'Female', annualSalary: 5500, dateOfBirth: '01/25/1985' }
         ];
-    }
-
-    trackByEmpCode(index: number, employee: any): string{
-        return employee.code;
-    }
-
-    removeEmployee(): void{
-        this.employees.splice(0,1); // always remove the first element
     }
 }

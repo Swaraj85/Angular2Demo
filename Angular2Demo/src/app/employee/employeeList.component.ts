46c99c59ce3e535c7f4adf98d116234ba487a836
Swaraj <ghoshswaraj2k@gmail.com>
2019-01-23 10:40:25 +0530

    Added usage of ngFor

---
+++ Angular2Demo/src/app/employee/employeeList.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: 'list-employee',
+    templateUrl: 'app/employee/employeeList.component.html',
+    styleUrls:['app/employee/employeeList.component.css']
+})
+
+export class EmployeeListComponent {
+    employees: any[] = [
+        { code: 'emp01', name: 'Tom', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/01/1985' },
+        { code: 'emp02', name: 'Tom2', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/01/1985' },
+        { code: 'emp03', name: 'Tom3', gender: 'Male', annualSalary: 5500, dateOfBirth: '25/01/1985' },
+        { code: 'emp04', name: 'Tom4', gender: 'Female', annualSalary: 5500, dateOfBirth: '25/01/1985' }
+    ];
+}
